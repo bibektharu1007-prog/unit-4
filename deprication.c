e43ac4db07f014be8d7d751994baf7cc3255e1e7
bibektharu1007-prog <bibektharu1007@gmail.com>
2026-02-23 20:34:22 +0545

    Update deprication.c

--- deprication.c
+++ deprication.c
@@ -2,8 +2,13 @@
 
 int main() {
     float cost, rate, dep;
+
+    printf("Enter cost and depreciation rate: ");
     scanf("%f %f", &cost, &rate);
+
     dep = cost * rate / 100;
-    printf("Depreciation = %.2f", dep);
+    printf("Depreciation = %.2f\n", dep);
+
     return 0;
 }
+
